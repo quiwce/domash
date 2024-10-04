ea5ca09c6d802766b6cfb5bb53b079b5a4582761
quiwce <ilkadegter23@gmail.com>
2024-10-04 12:10:20 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите символ от '0' до '9':");
+
+        while (true)
+        {
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine(); // Перенос на новую строку
+
+            if (char.IsDigit(input))
+            {
+                int result = charToNum(input);
+                Console.WriteLine($"Числовое значение: {result}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода. Пожалуйста, введите корректный символ от '0' до '9'.");
+            }
+        }
+
+        Console.WriteLine("Нажмите Enter, чтобы закрыть программу.");
+        Console.ReadLine();
+    }
+
+    public static int charToNum(char x)
+    {
+        return x - '0';
+    }
+}
